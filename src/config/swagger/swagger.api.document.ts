a20ead6954328431beae8ae13e392ad53dde568c
Hijiji <wlals1169@naver.com>
2024-06-13 20:05:51 +0900

    스웨거 연결

---
+++ src/config/swagger/swagger.api.document.ts
@@ -0,0 +1,24 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export class APIDocument {
+  public builder = new DocumentBuilder();
+
+  public initializeOptions(app: INestApplication) {
+    const options = this.builder
+      .setTitle('Dessert Time')
+      .setDescription('Dessert Time API Document')
+      .setVersion('1.0')
+      .addTag('DT')
+      .build();
+    //   .addBearerAuth({
+    //     type: 'http',
+    //     scheme: 'bearer',
+    //     bearerFormat: 'Token',
+    //   })
+
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('DessertTime', app, document);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as expressBasicAuth from 'express-basic-auth';
+import { APIDocument } from './config/swagger/swagger.api.document';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  // app.use(
+  //   ['/docs', '/docs-json'],
+  //   expressBasicAuth({
+  //     challenge: true,
+  //     users: { [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD },
+  //   }),
+  // );
+  new APIDocument().initializeOptions(app);
+
   await app.listen(3000);
+  
 }
 bootstrap();
