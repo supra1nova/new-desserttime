fbdd58e1eb4387555390d5c63812190f47122048
Hijiji <wlals1169@naver.com>
2024-06-17 23:57:41 +0900

    QnA Entity append on QnAModule

--- src/modules/qna/qna.repository.ts
+++ src/modules/qna/qna.repository.ts
@@ -7,17 +7,19 @@ import { QnAIdDto } from "./dto/qna.id.dto";
 
 @Injectable()
 export class QnARepository{
-    constructor(@InjectRepository(QnA) private qna:Repository<QnA>){
+    constructor(
+        @InjectRepository(QnA) 
+        private qnaRepository:Repository<QnA>){
     }
     async insertQnA(createQnADto:CreateQnADto){
-        await this.qna.insert(createQnADto);
+        await this.qnaRepository.insert(createQnADto);
     }
 
     async findQnAList(){
-       return await this.qna.find({select:{QnAId:true,content:true,email:true,createdDate:true},where:{usable:true}});
+       return await this.qnaRepository.find({select:{QnAId:true,content:true,email:true,createdDate:true},where:{usable:true}});
     }
 
     async findQnA(qnAIdDto:QnAIdDto){
-       return await this.qna.findOne({select:{QnAId:true,title:true,content:true,email:true,createdDate:true},where:{usable:true}});
+       return await this.qnaRepository.findOne({select:{QnAId:true,title:true,content:true,email:true,createdDate:true},where:{usable:true}});
     }
 }
