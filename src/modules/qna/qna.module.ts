939efef8043168579fccf6a602de93c4aada18a8
Hijiji <wlals1169@naver.com>
2024-06-17 23:49:27 +0900

    QnA Entity append on QnAModule

--- src/modules/qna/qna.module.ts
+++ src/modules/qna/qna.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { QnAController } from './qna.controller';
 import { QnAService } from './qna.service';
 import { QnARepository } from './qna.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QnA } from 'src/config/entities/qna.entity';
 
-@Module({
+@Module({    
+  imports: [
+  TypeOrmModule.forFeature([
+    QnA
+  ]),
+],
   controllers: [QnAController],
   providers: [QnAService,QnARepository]
 })
