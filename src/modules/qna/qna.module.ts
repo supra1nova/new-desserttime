a49155c56d981b7c2ffd037b420fdf30f42055fa
Hijiji <wlals1169@naver.com>
2024-06-17 23:46:19 +0900

    QnARepository QnAModule provisers에 등록

--- src/modules/qna/qna.module.ts
+++ src/modules/qna/qna.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QnAController } from './qna.controller';
 import { QnAService } from './qna.service';
+import { QnARepository } from './qna.repository';
 
 @Module({
   controllers: [QnAController],
-  providers: [QnAService]
+  providers: [QnAService,QnARepository]
 })
 export class QnAModule {}
