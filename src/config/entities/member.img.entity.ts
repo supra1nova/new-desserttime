5c5d43fac6bdbff27329d3c054f73c2f08630b25
supra1nova <supra1nova@gmail.com>
2024-07-27 23:53:48 +0900

    fix: Member entity 및 MemberImg entity 관련 수정
    - Member entity 관련, nickName nullable 설정 변경: 사용자 계정 생성시 nickname 은 무조건 생성됨
    - Member entity 관련, isUsable attribute 추가
    - MemberImg entity 관련, OneToOne 관계에 맞게 @JoinColumn 추가 설정
    - MemberImg entity 관련, isUsable attribute 추가

--- src/config/entities/member.img.entity.ts
+++ src/config/entities/member.img.entity.ts
@@ -1,4 +1,14 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, OneToOne, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  OneToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
 import { Member } from './member.entity';
 import { Review } from './review.entity';
 import { ReviewImg } from './review.img.entity';
@@ -20,12 +30,16 @@ export class MemberImg {
   @Column()
   imgName: string;
 
+  @Column({ nullable: true, default: true })
+  isUsable: boolean;
+
   @CreateDateColumn()
   createdDate: Date;
   
   @UpdateDateColumn()
   updateDate: Date;
 
-  @OneToOne(()=>Member, member => member.img)
-  member:Member
+  @OneToOne(() => Member, (member) => member.img)
+  @JoinColumn()
+  member: Member;
 }
