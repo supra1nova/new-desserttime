64b8927b1337b89504fa270a64031b5d8c96d7ef
Hijiji <wlals1169@naver.com>
2024-08-31 18:32:22 +0900

    middleware에서 req logging, resInterceptor에서 반환값 logging, loginterceptor 로그코드 삭제

--- src/config/interceptor/logger.interceptor.ts
+++ src/config/interceptor/logger.interceptor.ts
@@ -1,39 +1,18 @@
-import {
-    CallHandler,
-    ExecutionContext,
-    HttpException,
-    Injectable,
-    InternalServerErrorException,
-    Logger,
-    NestInterceptor,
-  } from '@nestjs/common';
-  import { Observable, catchError, tap } from 'rxjs';
-  import { DataSource } from 'typeorm';
+import { CallHandler, ExecutionContext, HttpException, Injectable, InternalServerErrorException, Logger, NestInterceptor } from '@nestjs/common';
+import { Observable, catchError, tap } from 'rxjs';
+import { DataSource } from 'typeorm';
 import { winstonLogger } from '../logger/winston.util';
-  
-  @Injectable()
-  export class LoggerInterceptor implements NestInterceptor {
-  
-    async intercept(
-      context: ExecutionContext,
-      next: CallHandler,
-    ): Promise<Observable<any>> {
-        const request = context.switchToHttp().getRequest();    
-        const { ip, method, originalUrl } = request;
-        const accessData = {
-            timestamp: new Date(),
-            originalUrl,
-            method: method,
-            ip,
-          };
-          winstonLogger.log({ accessData });
-  
-      return next.handle().pipe(
 
-        tap(async () => {
-
-        }),
-      );
-    }
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    const { ip, method, originalUrl } = request;
+    return next.handle().pipe(
+      catchError(async (error) => {
+        throw error;
+      }),
+      tap(async () => {}),
+    );
   }
-  
+}
--- src/config/interceptor/respons.interceptor.ts
+++ src/config/interceptor/respons.interceptor.ts
@@ -1,30 +1,31 @@
-import {
-    CallHandler,
-    ExecutionContext,
-    HttpException,
-    Injectable,
-    InternalServerErrorException,
-    Logger,
-    NestInterceptor,
-  } from '@nestjs/common';
-  import { Observable, catchError, tap } from 'rxjs';
-  import { DataSource } from 'typeorm';
+import { CallHandler, ExecutionContext, HttpException, Injectable, InternalServerErrorException, Logger, NestInterceptor } from '@nestjs/common';
+import { Observable, catchError, tap } from 'rxjs';
+import { DataSource } from 'typeorm';
 import { winstonLogger } from '../logger/winston.util';
 import { map } from 'rxjs/operators';
 
-  @Injectable()
-  export class ResponseInterceptor implements NestInterceptor {
-
-    async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
-        return next.handle().pipe(
-          map((data) => ({
-            success:true,
-            timestamp: new Date().toISOString(),
-            statusCode: context.switchToHttp().getResponse().statusCode,
-            message: 'Request successful',
-            data,
-          })),
-        );
-      }
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data) => {
+        const request = context.switchToHttp().getRequest();
+        const { ip, method, originalUrl } = request;
+        const resData = {
+          success: true,
+          timestamp: new Date().toISOString(),
+          statusCode: context.switchToHttp().getResponse().statusCode,
+          message: 'Request successful',
+          data,
+        };
+        winstonLogger.log({
+          ip,
+          method,
+          originalUrl,
+          resData,
+        });
+        return resData;
+      }),
+    );
   }
-  
+}
