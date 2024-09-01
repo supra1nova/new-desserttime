528aef4c1dd62729a10fc35e6bd96b6e9b58406b
Hijiji <wlals1169@naver.com>
2024-09-01 18:30:46 +0900

    문의하기 내용 하나 조회- 조건 추가

--- src/client-modules/qna/qna.repository.ts
+++ src/client-modules/qna/qna.repository.ts
@@ -1,25 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { QnA } from "src/config/entities/qna.entity";
-import { Repository } from "typeorm";
-import { CreateQnADto } from "./dto/create.qna.dto";
-import { QnAIdDto } from "./dto/qna.id.dto";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QnA } from 'src/config/entities/qna.entity';
+import { Repository } from 'typeorm';
+import { CreateQnADto } from './dto/create.qna.dto';
+import { QnAIdDto } from './dto/qna.id.dto';
 
 @Injectable()
-export class QnARepository{
-    constructor(
-        @InjectRepository(QnA) 
-        private qnaRepository:Repository<QnA>){
-    }
-    async insertQnA(createQnADto:CreateQnADto){
-        await this.qnaRepository.insert(createQnADto);
-    }
+export class QnARepository {
+  constructor(
+    @InjectRepository(QnA)
+    private qnaRepository: Repository<QnA>,
+  ) {}
+  async insertQnA(createQnADto: CreateQnADto) {
+    await this.qnaRepository.insert(createQnADto);
+  }
 
-    async findQnAList(){
-       return await this.qnaRepository.find({select:{qnaId:true,content:true,email:true,createdDate:true},where:{isUsable:true}});
-    }
+  async findQnAList() {
+    return await this.qnaRepository.find({ select: { qnaId: true, content: true, email: true, createdDate: true }, where: { isUsable: true } });
+  }
 
-    async findQnA(qnAIdDto:QnAIdDto){
-       return await this.qnaRepository.findOne({select:{qnaId:true,content:true,email:true,createdDate:true},where:{isUsable:true}});
-    }
+  async findQnA(qnAIdDto: QnAIdDto) {
+    return await this.qnaRepository.findOne({ select: { qnaId: true, content: true, email: true, createdDate: true }, where: { isUsable: true, qnaId: qnAIdDto.qId } });
+  }
 }
