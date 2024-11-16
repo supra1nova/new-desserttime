5d98c50f73ee865687303ec6972c136f5b404069
Hijiji <wlals1169@naver.com>
2024-11-16 17:41:00 +0900

    공지 조회 디스크립션 수정

--- src/client-modules/member/member.controller.ts
+++ src/client-modules/member/member.controller.ts
@@ -97,13 +97,13 @@ export class MemberController {
     return await this.memberService.getPointHisoryList(memberPointListDto);
   }
 
-  @ApiOperation({ summary: '공지/이벤트 목록 조회' })
+  @ApiOperation({ summary: '공지/이벤트/자주묻는질문 목록 조회' })
   @Get('my-page/notice/list/:noticeType')
   async getNoticeList(@Query() noticeListDto: NoticeListDto) {
     return await this.memberService.getNoticeList(noticeListDto);
   }
 
-  @ApiOperation({ summary: '공지/이벤트 하나 조회' })
+  @ApiOperation({ summary: '공지/이벤트/자주묻는질문 하나 조회' })
   @Get('my-page/notice/:noticeType/:noticeId')
   async getNoticeOne(@Query() noticeDto: NoticeDto) {
     return await this.memberService.getNoticeOne(noticeDto);
