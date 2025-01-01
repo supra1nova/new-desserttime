ab13e1afe0bda9edc96eb1f22c25d5e8f29744a3
Hijiji <wlals1169@naver.com>
2025-01-01 18:27:45 +0900

    메인이미지 확인값 수정

--- src/client-modules/review/dto/reviewimg.save.dto.ts
+++ src/client-modules/review/dto/reviewimg.save.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty } from 'class-validator';
 
 export class ReviewImgSaveDto {
@@ -15,6 +16,7 @@ export class ReviewImgSaveDto {
     description: '메인사진이면 true',
     required: true,
   })
+  @Transform(({ value }) => (value === 'true' ? true : false)) // 문자열 "true"를 boolean true로 변환
   @IsNotEmpty()
   readonly isMain: boolean;
 
